1b9a33692098c679084ec86f2a64188524e06ee8
Madhav <madhavAO321F@gmail.com>
2026-07-15 14:31:45 +0530

    refactor: remove custom CORS middleware in favor of standard configuration

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 const PORT = process.env.PORT || 3000;
-
-// Body parser
 app.use(express.json());
-
-// Custom CORS middleware
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-    next();
-});
 
 const dbconnect = require("./database/db");
 dbconnect();
